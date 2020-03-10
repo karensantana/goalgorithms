e4215925a222dc8ee6466c1b068c51a441ab6321
Karen Santana <itt.karensantana@gmail.com>
2020-03-10 14:12:56 -0400

    Adding ReverseStringRecursively

--- scr/algorithms/reverse_string.go
+++ scr/algorithms/reverse_string.go
@@ -1,11 +1,24 @@
 package algorithms
 
-//ReverseString: takes an string and returns it reversed
-func ReverseString(str string) string {
+import "strings"
+
+//ReverseStringRecursively: takes an string and returns it reversed
+func ReverseStringRecursively(str string) string {
 	if len(str) == 0 {
 		return ""
 	} else if len(str) == 1 {
 		return string(str[0])
 	}
 	return string(str[len(str)-1]) + ReverseString(str[0:(len(str)-1)])
+}
+
+//ReverseString
+func ReverseString(str string) string {
+	var sb strings.Builder
+
+	for i := len(str) - 1; i >= 0; i-- {
+		sb.WriteByte(str[i])
+	}
+
+	return sb.String()
 }
